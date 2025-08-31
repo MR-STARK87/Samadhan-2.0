88c0090cb6a79be6537a82d160188010d9ea7181
Shekh Sahil <65450296+theinnocentboy@users.noreply.github.com>
2025-08-31 23:54:17 +0530

    Add files via upload

---
+++ Day 16/Day16-Sahil/models/Order.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  items: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      quantity: { type: Number },
+    },
+  ],
+  total: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
